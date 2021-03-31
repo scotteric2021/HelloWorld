f4ed42a332669b8587df3fc81b19e32bed24c9e3
Eric Scott <scotteric2021@gmail.com>
2021-03-30 19:57:34 -0500

    Now we can run it

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,12 +3,8 @@ using namespace std;
 
 int main()
 {
-    int entry;  //Initalize an integer for input to exit
+
     cout << "Hello World from C++ in VS Code";
-    cin >> entry;
-    if (entry < 0)
-    {
-        break;
-    }
+    
 return 0;
 }
