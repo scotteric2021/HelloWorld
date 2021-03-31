bfe9a5c8c09cbad784a81acca327fe1ed3c12d0f
Eric Scott <scotteric2021@gmail.com>
2021-03-30 19:26:00 -0500

    Now we have some code

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int entry;  //Initalize an integer for input to exit
+    cout << "Hello World from C++ in VS Code";
+    cin >> entry;
+    if (entry < 0)
+    {
+        break;
+    }
+return 0;
+}
